8431b5bff57e1d7545db3d7b5bdfb33902504c78
4keool <dodocsang@gmail.com>
2022-05-15 23:27:44 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,11 +5,11 @@ const express = require('express');
 const app = express();
 
 // Routing
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 
 
 // Setting App
-app.set("views", "./views")
+app.set("views", "./src/views")
 app.set("view engine", "ejs")
 
 app.use("/", home)
